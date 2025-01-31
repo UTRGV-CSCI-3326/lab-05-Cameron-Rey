3dde2bececcb06847cfcb7dab6f83dd131d06e94
Cameron Borrego <161384565+Cameron-Rey@users.noreply.github.com>
2025-01-31 02:54:28 +0000

    5th lab and commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Input {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter user's name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter user's age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter user's weight: ");
+        float weight = scanner.nextFloat();
+
+        scanner.nextLine();
+
+        System.out.print("(True or False) You are a smoker? ");
+        boolean smokerStatus = scanner.nextBoolean();
+
+        System.out.println("User's name: " + name + ", User's age: " + age + ", User's weight: " + weight + ",  You are a smoker? " + smokerStatus);
+
+        scanner.close();
+    }
+
+}
